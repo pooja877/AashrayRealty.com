6a7c09bb41fdbe5a50a5ac68f43cc1a9f1a8631b
pooja877 <159251631+pooja877@users.noreply.github.com>
2025-01-17 23:39:00 +0530

    add changes

--- client/src/routes/Signin/Signin.jsx
+++ client/src/routes/Signin/Signin.jsx
@@ -2,7 +2,7 @@ import "./signin.scss"
 import { useState } from "react";
 import {Link,useNavigate} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
-import {signInStart,signInSuccess,signInFailure} from '../redux/user/userSlice.js';
+import {signInStart,signInSuccess,signInFailure} from '../redux/user/userSlice';
 
 function SignIn()
 {   
