f60a019326a85e84caa56b464c284093bc5d7a0c
pooja877 <159251631+pooja877@users.noreply.github.com>
2025-01-19 18:08:44 +0530

    add changes

--- client/src/components/edit/Edit.jsx
+++ client/src/components/edit/Edit.jsx
@@ -1,18 +1,57 @@
 import './Edit.scss'
 import { useSelector } from 'react-redux';
+import {  useState } from 'react';
+import { updateUserStart ,updateUserSuccess,updateUserFailure} from '../../redux/user/userSlice.js';
+import { useDispatch } from 'react-redux';
 export default function Edit() {
-    const {currentUser}=useSelector((state)=>state.user);
+
+    const {currentUser, loading}=useSelector((state)=>state.user);
+    const [formData, setFormData] = useState({});
+   // const [updateSuccess, setUpdateSuccess] = useState(false);
+    const dispatch=useDispatch();
+    console.log(formData);
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.id]: e.target.value });
+    };
+
+    const handleSubmit=async (e)=>{
+      e.preventDefault();
+      try{
+        dispatch(updateUserStart());
+        const res = await fetch(`/api/user/update/${currentUser._id}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await res.json();
+        if (data.success === false) {
+          dispatch(updateUserFailure(data.message));
+          return;
+        }
+        dispatch(updateUserSuccess(data));
+       // setUpdateSuccess(true);
+      }
+      catch(error)
+      {
+        dispatch(updateUserFailure(error.message));
+        
+      }
+    }
   return (
     <div className='main-container'>
-        
         <div className="container">
-        <img src={currentUser.avatar}/>
-         <form>
-           Username: <input type="text" placeholder={currentUser.username} id="username"/>
-           Email: <input type="email" placeholder={currentUser.email} id='email' /> 
+        <img src={currentUser.avatar} action=""/>
+         <form onSubmit={handleSubmit}>
+           Username: <input type="text" placeholder={currentUser.username} id="username"onChange={handleChange}/>
+           Email: <input type="email" placeholder={currentUser.email} id='email' onChange={handleChange}/> 
+           <button  disabled={loading}>
+           {loading ? 'Loading...' : 'Save Changes'}
+           </button>
          </form>
         
-         <button className='btnChanges'>Save Changes</button>
+        
        
         </div>
      </div>
--- client/src/routes/profile/Profile.jsx
+++ client/src/routes/profile/Profile.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import {Link} from "react-router-dom";
 export default function Profile() {
     const {currentUser}=useSelector((state)=>state.user);
+    
      return (
       
     <div className='infocontainer'>
